12af5ff4b3c03bc34787f04cd1668703f6c6dd21
Andxyz <anderson_x123@hotmail.com>
2025-02-13 01:06:20 -0300

    GeradorInserts: método obtem_tipo_por_id

--- script/gerador_inserts.py
+++ script/gerador_inserts.py
@@ -15,66 +15,6 @@ class GeradorInserts:
         self.__df_origem_dados = None
         self.__df_tipos = None
 
-    def __descompactar_arquivo(self, nome_arquivo_zip: str) -> bool:
-        """Descompacta o arquivo zip com os dados de origem.
-
-        Args:
-            nome_arquivo_zip (str): Nome do arquivo zip a ser descompactado.
-
-        Returns:
-            bool: True se a operação for bem sucedida, False caso contrário.
-        """
-        arquivos_diretorio_script = os.listdir(self.PATH_DADOS)
-
-        # Verifica se os arquivos já foram descompactados
-        if (any(
-            arquivo in self.ARQUIVOS_CSV
-            for arquivo in arquivos_diretorio_script
-        )):
-            print('Arquivos já descompactados.')
-            return True
-
-        # Descompacta o arquivo zip
-        try:
-            with (ZipFile(
-                file = f"{self.PATH_DADOS}/{nome_arquivo_zip}",
-                mode = 'r'
-            ) as zip_ref):
-                zip_ref.extractall(self.PATH_DADOS)
-
-        except Exception as e:
-            print(f"Erro ao descompactar arquivo: {e}")
-
-            # Exibe os arquivos do diretório /script
-            print(f"Arquivos no diretório {self.PATH_DADOS}:")
-            print(os.listdir(self.PATH_DADOS))
-
-            return False
-
-        return True
-
-    def __carregar_arquivos_csv(self) -> bool:
-        """Carrega os arquivos CSV de origem dos dados e os tipos.
-
-        Returns:
-            bool: True se a operação for bem sucedida, False caso contrário.
-        """
-        # Carrega os arquivos CSV
-        try:
-            self.__df_origem_dados = pd.read_csv(
-                f"{self.PATH_DADOS}/{self.ARQUIVOS_CSV[0]}"
-            )
-
-            self.__df_tipos = pd.read_csv(
-                f"{self.PATH_DADOS}/{self.ARQUIVOS_CSV[1]}"
-            )
-
-        except Exception as e:
-            print(f"Erro ao carregar arquivos CSV: {e}")
-            return False
-
-        return True
-
     def __ordenar_filtrar_dados(self) -> bool:
         """Efetua a ordenação dos dados e filtra apenas
             os registros com status "CRITICO".
@@ -155,15 +95,91 @@ class GeradorInserts:
 
         return query_select
 
+    def descompactar_arquivo(self, nome_arquivo_zip: str) -> bool:
+        """Descompacta o arquivo zip com os dados de origem.
+
+        Args:
+            nome_arquivo_zip (str): Nome do arquivo zip a ser descompactado.
+
+        Returns:
+            bool: True se a operação for bem sucedida, False caso contrário.
+        """
+        arquivos_diretorio_script = os.listdir(self.PATH_DADOS)
+
+        # Verifica se os arquivos já foram descompactados
+        if (any(
+            arquivo in self.ARQUIVOS_CSV
+            for arquivo in arquivos_diretorio_script
+        )):
+            print('Arquivos já descompactados.')
+            return True
+
+        # Descompacta o arquivo zip
+        try:
+            with (ZipFile(
+                file = f"{self.PATH_DADOS}/{nome_arquivo_zip}",
+                mode = 'r'
+            ) as zip_ref):
+                zip_ref.extractall(self.PATH_DADOS)
+
+        except Exception as e:
+            print(f"Erro ao descompactar arquivo: {e}")
+
+            # Exibe os arquivos do diretório /script
+            print(f"Arquivos no diretório {self.PATH_DADOS}:")
+            print(os.listdir(self.PATH_DADOS))
+
+            return False
+
+        return True
+
+    def carregar_arquivos_csv(self) -> bool:
+        """Carrega os arquivos CSV de origem dos dados e os tipos.
+
+        Returns:
+            bool: True se a operação for bem sucedida, False caso contrário.
+        """
+        # Carrega os arquivos CSV
+        try:
+            self.__df_origem_dados = pd.read_csv(
+                f"{self.PATH_DADOS}/{self.ARQUIVOS_CSV[0]}"
+            )
+
+            self.__df_tipos = pd.read_csv(
+                f"{self.PATH_DADOS}/{self.ARQUIVOS_CSV[1]}"
+            )
+
+        except Exception as e:
+            print(f"Erro ao carregar arquivos CSV: {e}")
+            return False
+
+        return True
+
+    def obtem_tipo_por_id(self, id_tipo: int) -> str:
+        """Obtém a descrição do tipo pelo id.
+
+        Args:
+            id_tipo (int): id do tipo consultado.
+
+        Returns:
+            str: Descrição do tipo.
+        """
+        tipo = self.__df_tipos.loc[
+            self.__df_tipos['id'] == id_tipo,
+            'nome'
+        ]
+
+        return tipo.values[0] if not tipo.empty else None
+
     def executar(self):
         """Executa o script que efetua a descompactação do arquivo .zip
             leitura dos arquivos .csv descompactados, tratamento dos dados
             geração dos inserts SQL e, ao final, a query de consulta.
         """
-        if not self.__descompactar_arquivo("dados.zip"):
+        if not self.descompactar_arquivo("dados.zip"):
             return
 
-        if not self.__carregar_arquivos_csv():
+        if not self.carregar_arquivos_csv():
             return
 
         if not self.__ordenar_filtrar_dados():
