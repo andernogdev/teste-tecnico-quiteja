6e0d0b2a00cccb869b33c58697cc9c022e141f03
Andxyz <anderson_x123@hotmail.com>
2025-02-13 01:24:58 -0300

    main.py rodando API Flask

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from api.routes import app
 from script.gerador_inserts import GeradorInserts
 
 if __name__ == '__main__':
     # Executar o script
     gerador_inserts = GeradorInserts()
     gerador_inserts.executar()
+
+    # Descomentar para rodar a API
+    app.run(debug=True)
